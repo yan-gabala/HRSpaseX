692fd2e9c71928cccf9096751aceaf955ba37220
JuliSem <julia.sem295@yandex.ru>
2024-03-18 20:19:42 +0300

    Chore: отредактировала админ панель заявки и связанных моделей

--- hrspace/orders/admin.py
+++ hrspace/orders/admin.py
@@ -4,36 +4,28 @@ from orders.models import (BenefitsPackage, City, HrResponsibility, Order,
                            Profession, Skill, TypeEmployment)
 
 
-@admin.register(BenefitsPackage)
-class BenefitsPackageAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(City)
-class CityAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(HrResponsibility)
-class HrResponsibilityAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'name', 'get_profession', 'get_city', 'work_format',
+        'salary_from', 'salary_to'
+    )
+    list_filter = ('profession', 'city', 'work_format')
+
+    def get_profession(self, row):
+        return ','.join([obj.name for obj in row.profession.all()])
+
+    get_profession.short_description = 'Профессия'
+
+    def get_city(self, row):
+        return ','.join([obj.name for obj in row.city.all()])
+
+    get_city.short_description = 'Город'
 
 
-@admin.register(Profession)
-class ProfessionAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Skill)
-class SkillAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(TypeEmployment)
-class TypeEmploymentAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(BenefitsPackage)
+admin.site.register(City)
+admin.site.register(HrResponsibility)
+admin.site.register(Profession)
+admin.site.register(Skill)
+admin.site.register(TypeEmployment)
