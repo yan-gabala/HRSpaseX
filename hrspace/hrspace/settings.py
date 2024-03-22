61f79e37e3d84cf2a7d420e196dc12493121c81c
Eduard Gumen <hydrospirt@mail.ru>
2024-03-22 13:07:45 +0300

    feature: Add core app

--- hrspace/hrspace/settings.py
+++ hrspace/hrspace/settings.py
@@ -16,9 +16,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular'
     'api.apps.ApiConfig',
     'orders.apps.OrdersConfig',
-    'drf_spectacular',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
