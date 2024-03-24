f6adba89db95496498d7b528f2175d3f376be591
JuliSem <julia.sem295@yandex.ru>
2024-03-24 16:10:20 +0300

    Chore: подправила поля модели заявки + отображение их в сериализаторе

--- hrspace/api/serializers.py
+++ hrspace/api/serializers.py
@@ -23,6 +23,18 @@ class LineOfBusinessSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
 
+    line_of_business = serializers.StringRelatedField(many=False)
+    city = serializers.StringRelatedField(many=False)
+    skill = serializers.StringRelatedField(many=True)
+    salary_from = serializers.StringRelatedField()
+    salary_to = serializers.StringRelatedField()
+    amount_of_subordinate = serializers.StringRelatedField()
+    amount_of_employees = serializers.StringRelatedField()
+    award_option = serializers.StringRelatedField()
+    award = serializers.StringRelatedField()
+    format_interview = serializers.StringRelatedField()
+    amount_of_hr = serializers.StringRelatedField()
+
     class Meta:
         model = Order
         fields = ('id', 'name', 'line_of_business', 'city', 'work_format',
@@ -32,4 +44,6 @@ class OrderSerializer(serializers.ModelSerializer):
                   'work_experience', 'skill', 'education', 'portfolio',
                   'amount_of_employees', 'award_option', 'award', 'start_work',
                   'format_interview', 'start_interview', 'amount_of_hr',
-                  'hr_responsibility', 'hr_requirements')
+                  'hr_responsibility1', 'hr_responsibility2', 'hr_responsibility3',
+                  'hr_responsibility4', 'hr_responsibility5', 'hr_requirements',
+                  'hr_requirements1', 'hr_requirements2')
