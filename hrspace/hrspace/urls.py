c4639c054a40fd2eace5844280f085d8dc8b58c4
Serhii Vinogradov <123404466+yan-gabala@users.noreply.github.com>
2024-03-24 14:45:04 +0300

    ci: url for path

--- hrspace/hrspace/urls.py
+++ hrspace/hrspace/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', admin.site.urls),
 ]
+
